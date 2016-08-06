b1fb5c64c6a266bae236dd6818dddb075d612a04
Ken V <kvaczi2@gmail.com>
2016-08-06 10:19:53 -0500

    Added basic countdown functionality

--- Launch Codes/ViewController.swift
+++ Launch Codes/ViewController.swift
@@ -9,10 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var disarmCode = 0
+    var guessesLeft = 0
+    
+    @IBOutlet weak var codeEnterTextField: UITextField!
+    @IBOutlet weak var guessResponseLabel: UILabel!
+    @IBOutlet weak var countdownTimerLabel: UILabel!
+    
+    @IBAction func guessButtonTap(_ sender: UIButton) {
+        if !(codeEnterTextField.text ?? "").isEmpty {
+            if (codeEnterTextField.text! as NSString).integerValue == disarmCode {
+                //Disarm
+                guessResponseLabel.text = "Congratulations!!"
+                countdownTimerLabel.text = "You stopped the launch"
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5)) {
+                    self.initializeCountdown()
+                }
+            } else if guessesLeft > 1 {
+                //Wrong
+                guessResponseLabel.text = "Nope!"
+                guessesLeft -= 1
+                countdownTimerLabel.text = String(guessesLeft)
+            } else {
+                //Boom
+                let countriesPotentiallyDestroyed = ["Brazil", "Russia", "USA", "Croatia", "Australia"] //Random countries
+                guessResponseLabel.text = "Oops!!"
+                countdownTimerLabel.text = "You destroyed \(countriesPotentiallyDestroyed[Int(arc4random_uniform(UInt32(countriesPotentiallyDestroyed.count)))])"
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5)) {
+                    self.initializeCountdown()
+                }
+            }
+        }
+    }
+    
+    func initializeCountdown() {
+        disarmCode = Int(arc4random_uniform(10)) + 1 //random number 1-10
+        guessesLeft = 6
+        countdownTimerLabel.text = String(guessesLeft)
+        guessResponseLabel.text = ""
+        codeEnterTextField.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initializeCountdown()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +65,3 @@ class ViewController: UIViewController {
 
 
 }
-
