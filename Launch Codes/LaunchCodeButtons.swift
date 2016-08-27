1f38450327455d45ceead77669061d667bdb2417
Ken V <kvaczi2@gmail.com>
2016-08-27 08:22:00 -0500

    initial working prototype

---
+++ Launch Codes/LaunchCodeButtons.swift
@@ -0,0 +1,59 @@
+//
+//  LaunchCodeButton.swift
+//  Launch Codes
+//
+//  Created by KENNETH VACZI on 8/22/16.
+//  Copyright © 2016 KENNETH VACZI. All rights reserved.
+//
+
+import UIKit
+
+class LaunchCodeButton: UIButton {
+    
+    var originPoint: CGPoint?
+    var currentGuessPosition: Int?
+    let animationDuration = 0.3
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        //self.addTarget(nil, action: Selector(("guessButtonTap:")), for: UIControlEvents.touchUpInside)
+        print("initializing button \(self.titleLabel?.text)")
+    }
+    
+    public func moveToPoint(_ destinationPoint: CGPoint, destinationGuessPosition: Int) {
+        if self.originPoint == nil {
+            self.originPoint = self.frame.origin
+        }
+        UIView.animate(withDuration: animationDuration, animations: { () -> Void in
+            self.frame.origin = CGPoint(x: destinationPoint.x, y: destinationPoint.y)
+        })
+        currentGuessPosition = destinationGuessPosition
+    }
+    
+    public func moveBackToOrigin () {
+        if originPoint != nil {
+            UIView.animate(withDuration: animationDuration, animations: { () -> Void in
+                self.frame.origin = self.originPoint!
+            })
+            currentGuessPosition = nil
+        }
+    }
+    
+    public func isAtOrigin() -> Bool {
+        if self.originPoint == nil || self.originPoint == self.frame.origin {
+            print("at origin")
+            return true
+        }
+        print("not at origin")
+        print(self.originPoint)
+        return false
+    }
+    
+    public func label() -> String {
+        if let text = self.titleLabel?.text {
+            return text
+        }
+        return ""
+    }
+    
+}
--- Launch Codes/LaunchCodesViewController.swift
+++ Launch Codes/LaunchCodesViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class LaunchCodesViewController: UIViewController {
-
+    
+    @IBOutlet weak var codeLabelsLabel: UILabel!
     @IBOutlet weak var code1Label: LaunchCodeLabel!
     @IBOutlet weak var code2Label: LaunchCodeLabel!
     @IBOutlet weak var code3Label: LaunchCodeLabel!
     @IBOutlet weak var code4Label: LaunchCodeLabel!
     
+    @IBOutlet weak var guessButtonsLabel: UILabel!
     @IBOutlet weak var guess1Button: LaunchCodeButton!
     @IBOutlet weak var guess2Button: LaunchCodeButton!
     @IBOutlet weak var guess3Button: LaunchCodeButton!
@@ -24,37 +26,9 @@ class LaunchCodesViewController: UIViewController {
     @IBOutlet weak var guess7Button: LaunchCodeButton!
     @IBOutlet weak var guess8Button: LaunchCodeButton!
     
-    @IBAction func guess1ButtonTap(_ sender: LaunchCodeButton) {
-        guessButtonTap(sender)
-    }
-    @IBAction func guess2ButtonTap(_ sender: LaunchCodeButton) {
-        guessButtonTap(sender)
-    }
-    @IBAction func guess3ButtonTap(_ sender: LaunchCodeButton) {
-        guessButtonTap(sender)
-    }
-    @IBAction func guess4ButtonTap(_ sender: LaunchCodeButton) {
-        guessButtonTap(sender)
-    }
-    @IBAction func guess5ButtonTap(_ sender: LaunchCodeButton) {
-        guessButtonTap(sender)
-    }
-    @IBAction func guess6ButtonTap(_ sender: LaunchCodeButton) {
-        guessButtonTap(sender)
-    }
-    @IBAction func guess7ButtonTap(_ sender: LaunchCodeButton) {
-        guessButtonTap(sender)
-    }
-    @IBAction func guess8ButtonTap(_ sender: LaunchCodeButton) {
-        guessButtonTap(sender)
-    }
+    @IBOutlet weak var countdownTimerLabel: UILabel!
     
-    var guessList: [String?] = []
-    var guessButtons: [LaunchCodeButton] = []
-    var codeLabels: [LaunchCodeLabel] = []
-    var code = LaunchCode()
-    
-    func guessButtonTap(_ sender: LaunchCodeButton) {
+    @IBAction func guessButtonTap(_ sender: LaunchCodeButton) {
         print("tapped guess \(sender.label())")
         if sender.isAtOrigin() {
             if let firstSlot = firstAvailableGuessSlot() {
@@ -68,6 +42,18 @@ class LaunchCodesViewController: UIViewController {
             }
         }
     }
+
+    var guessList: [String?] = []
+    var guessButtons: [LaunchCodeButton] = []
+    var codeLabels: [LaunchCodeLabel] = []
+    var code = LaunchCode()
+    var countdownSecondsLeft: Int = 60
+    var countdown = Timer()
+    
+    let guessButtonBackgroundColor = UIColor(white: 0.9, alpha: 1)
+    let guessButtonTextColor = UIColor(white: 0.25, alpha: 1)
+    let guessButtonCorrectTextColor = UIColor.green
+    
     
     func firstAvailableGuessSlot() -> Int? {
         for (index, guess) in guessList.enumerated() {
@@ -95,29 +81,66 @@ class LaunchCodesViewController: UIViewController {
     
     func testCompleteCodeAndReturnIncorrects() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            var codeIsCorrect = true
             for (index, codeSegmentCheck) in self.code.checkCode(self.guessList).enumerated() {
                 if codeSegmentCheck == true {
-                    print ("code segment in slot \(index) is correct")
+                    for guessButton in self.guessButtons {
+                        if guessButton.currentGuessPosition == index {
+                            print("code segment in slot \(index) is correct")
+                            guessButton.backgroundColor = nil
+                            guessButton.titleLabel?.textColor = self.guessButtonCorrectTextColor
+                        }
+                    }
                 } else {
                     for guessButton in self.guessButtons {
                         if guessButton.currentGuessPosition == index {
                             print("code segment in slot \(index) is incorrect")
                             self.guessList[guessButton.currentGuessPosition!] = nil
                             guessButton.moveBackToOrigin()
+                            codeIsCorrect = false
+                            guessButton.backgroundColor = self.guessButtonBackgroundColor
+                            guessButton.titleLabel?.textColor = self.guessButtonTextColor
                         }
                     }
                 }
             }
+            if codeIsCorrect == true {
+                self.winGame()
+            }
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+    func winGame() {
+        print("congratulations you won")
+        countdown.invalidate()
+        resetLaunchCode()
+    }
+    
+    func loseGame() {
+        print("congratulations you lost")
+        countdown.invalidate()
+        resetLaunchCode()
+    }
+    
+    func decreaseCountdown() {
+        print ("decreasing countdown")
+        countdownSecondsLeft -= 1
+        countdownTimerLabel.text = String(countdownSecondsLeft)
+        if countdownSecondsLeft <= 0 {
+            loseGame()
+        }        
+    }
+    
+    func initializeCountdown() {
+        countdown = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.decreaseCountdown), userInfo: nil, repeats: true)
+    }
+    
+    func resetLaunchCode() {
         code = LaunchCode()
         guessList = [nil, nil, nil, nil]
         guessButtons = []
+        codeLabels = []
+        countdownSecondsLeft = 60
         
         code1Label.text = ""
         code2Label.text = ""
@@ -142,8 +165,43 @@ class LaunchCodesViewController: UIViewController {
             guessButtons[index].setTitle(guess, for: .normal)
         }
         
+        for guessButton in guessButtons {
+            guessButton.backgroundColor = guessButtonBackgroundColor
+            guessButton.titleLabel?.textColor = guessButtonTextColor
+        }
+        
+        initializeCountdown()
+        
+        arrangeLayout()
     }
-
+    
+    func arrangeLayout () {
+        let buttonWidth = (self.view.frame.width / CGFloat(code.guessListLength + 2)) * 2
+        let buttonHeight = CGFloat((guess1Button.titleLabel?.font.lineHeight)!) * 1.5
+        let buttonSpacingX = (self.view.frame.width - buttonWidth * CGFloat(code.guessListLength / 2)) / CGFloat(code.guessListLength / 2 + 1)
+        let firstRowSpacingY = self.view.frame.height - buttonHeight * 5
+        let secondRowSpacingY = firstRowSpacingY + buttonHeight * 1.5
+        for (index, button) in guessButtons[0...3].enumerated() {
+            button.frame = CGRect(x: (buttonSpacingX + buttonWidth) * CGFloat(index) + buttonSpacingX, y: firstRowSpacingY, width: buttonWidth, height: buttonHeight)
+        }
+        for (index, button) in guessButtons[4...7].enumerated() {
+            button.frame = CGRect(x: (buttonSpacingX + buttonWidth) * CGFloat(index) + buttonSpacingX, y: secondRowSpacingY, width: buttonWidth, height: buttonHeight)
+        }
+        let codeSpacingY = buttonHeight * 4
+        for (index, label) in codeLabels[0...3].enumerated() {
+            label.frame = CGRect(x: (buttonSpacingX + buttonWidth) * CGFloat(index) + buttonSpacingX, y: codeSpacingY, width: buttonWidth, height: buttonHeight)
+            label.backgroundColor = nil
+        }
+        guessButtonsLabel.frame = CGRect(x: buttonSpacingX, y: firstRowSpacingY - buttonHeight, width: self.view.frame.width, height: buttonHeight)
+        codeLabelsLabel.frame = CGRect(x: buttonSpacingX, y: codeSpacingY - buttonHeight, width: self.view.frame.width, height: buttonHeight)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        resetLaunchCode()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
